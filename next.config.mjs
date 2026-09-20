46cdd5e34c1ea0cd5208b54dbaa24ace417e3e66
Abi Prospa <126681430+AbiProspa@users.noreply.github.com>
2026-09-20 07:17:12 +0100

    Add new webpack cache files for client and server development builds
    - Introduced new compressed pack files for client and server development in the webpack cache.
    - These files are essential for optimizing build performance and improving load times during development.

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingExcludes: {
-    "*": ["./Ashley html template/**", "./__MACOSX/**"],
-  },
-};
-
-export default nextConfig;
